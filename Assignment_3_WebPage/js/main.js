d2e06cbbd5796131e7df7d9109eed32a49ed8756
HuyNzzz <a1984883@adelaide.edu.au>
2026-04-27 10:55:26 +0930

    fix: Fix image cycler bug and replace var with const and let

--- Assignment_3_WebPage/js/main.js
+++ Assignment_3_WebPage/js/main.js
@@ -2,17 +2,18 @@
 // Cycles through study images when the Next Image button is clicked
 
 // List of image file paths to cycle through
-var images = ["assets/images/study1.jpg", "assets/images/study2.jpg", "assets/images/study3.jpg"];
-
-// Track which image is currently showing
-var current = 0;
+const images = ["assets/images/study1.jpg", "assets/images/study2.jpg", "assets/images/study3.jpg"];
 
 // Get the image element and button from the page
-var img = document.getElementById("studyImage");
-var btn = document.getElementById("cycleBtn");
+const img = document.getElementById("studyImage");
+const btn = document.getElementById("cycleBtn");
+
+// Track which image is currently showing
+let current = 0;
 
 // When the button is clicked, move to the next image
 btn.onclick = function() {
-    current = current + 1;
+    // Loop back to the first image after the last one
+    current = (current + 1) % images.length;
     img.src = images[current];
 }
